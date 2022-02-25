f5775581d9ff6158c5014bb592567167178650ed
ArshS1 <90710790+ArshS1@users.noreply.github.com>
2022-02-25 17:34:32 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,89 @@
+var information;
+function inputChecker() {
+  information = document.getElementById("textContainer").value;
+  console.log(information);
+}
+
+// switch (key) {
+//     case 'A':
+
+//         break;
+//     case 'B':
+
+//         break;
+//     case 'C':
+
+//         break;
+//     case 'D':
+
+//         break;
+//     case 'E':
+
+//         break;
+//     case 'F':
+
+//         break;
+//     case 'G':
+
+//         break;
+//     case 'H':
+
+//         break;
+//     case 'I':
+
+//         break;
+//     case 'J':
+
+//         break;
+//     case 'K':
+
+//         break;
+//     case 'L':
+
+//         break;
+//     case 'M':
+
+//         break;
+//     case 'N':
+
+//         break;
+//     case 'O':
+
+//         break;
+//     case 'P':
+
+//         break;
+//     case 'Q':
+
+//         break;
+//     case 'R':
+
+//         break;
+//     case 'S':
+
+//         break;
+//     case 'T':
+
+//         break;
+//     case 'U':
+
+//         break;
+//     case 'V':
+
+//         break;
+//     case 'W':
+
+//         break;
+//     case 'X':
+
+//         break;
+//     case 'Y':
+
+//         break;
+//     case 'Z':
+
+//         break;
+
+//     default:
+//         break;
+// }
